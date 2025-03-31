b7bf7ac0b6522a19ca78a3b95379e5590e1fb922
bjekovicn <n.bjekovic97@gmail.com>
2025-03-31 14:59:02 +0200

    Get Category By Id Query

---
+++ AssociationsClean.Application/Features/Categories/GetCategoryById/GetCategoryByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+using AssociationsClean.Domain;
+using AssociationsClean.Application.Abstractions.Messaging;
+
+namespace Associations.Application.Features.Categories.GetCategories
+{
+    public sealed record GetCategoryByIdQuery(int Id):IQuery<Category>;
+}
---
+++ AssociationsClean.Application/Features/Categories/GetCategoryById/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿
+
+using AssociationsClean.Domain;
+using AssociationsClean.Domain.Abstractions;
+using AssociationsClean.Application.Abstractions.Data;
+using AssociationsClean.Application.Abstractions.Messaging;
+using Dapper;
+
+namespace Associations.Application.Features.Categories.GetCategories
+{
+
+    internal sealed class GetCategoryByIdQueryHandler:IQueryHandler<GetCategoryByIdQuery,Category>
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public GetCategoryByIdQueryHandler(ISqlConnectionFactory connectionFactory)
+        {
+            this._sqlConnectionFactory = connectionFactory;
+        }
+
+        public async Task<Result<Category>> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            using var connection=_sqlConnectionFactory.CreateConnection();
+
+            var sql = "SELECT Id, Name, Photo FROM Categories WHERE Id = @Id";
+
+            var category = await connection.QueryFirstOrDefaultAsync<Category>(sql, new { Id = request.Id });
+
+
+            return category;
+        }
+    }
+}
