853120bbd28c88fb4dc115395b0e527190019602
bjekovicn <n.bjekovic97@gmail.com>
2025-04-13 19:02:37 +0200

    associations repositories

---
+++ AssociationsClean.Application/Features/Associations/IAssociationQueryRepository.cs
@@ -0,0 +1,9 @@
+﻿using AssociationsClean.Application.Shared.Abstractions.Repositories;
+using AssociationsClean.Domain.Features.Associations;
+
+namespace AssociationsClean.Application.Features.Associations
+{
+    public interface IAssociationQueryRepository: IBaseQueryRepository<Association>
+    {
+    }
+}
---
+++ AssociationsClean.Infrastructure/Features/Associations/AssociationQueryRepository.cs
@@ -0,0 +1,34 @@
+﻿using AssociationsClean.Domain.Features.Associations;
+using AssociationsClean.Application.Features.Associations;
+using AssociationsClean.Application.Shared.Abstractions.Data;
+using Dapper;
+
+namespace AssociationsClean.Infrastructure.Features.Associations
+{
+    internal sealed class AssociationQueryRepository : IAssociationQueryRepository
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public AssociationQueryRepository(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public async Task<IReadOnlyList<Association>> GetAllAsync()
+        {
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            var sql = "SELECT * FROM public.\"Associations\""; 
+
+            var result = await connection.QueryAsync<Association>(sql);
+            return result.AsList();
+        }
+
+        public async Task<Association?> GetByIdAsync(int id)
+        {
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            var sql = "SELECT * FROM public.\"Associations\" WHERE \"Id\" = @Id";
+
+            return await connection.QueryFirstOrDefaultAsync<Association>(sql, new { Id = id });
+        }
+    }
+}
